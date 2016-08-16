a1c3886dc7513d55cc18afcb04dff9c642a94686
sidi kasmi <sidi.kasmi@pickit.com>
2016-08-16 18:03:44 +0200

    fix slider for event handler

--- static/js/slider.js
+++ static/js/slider.js
@@ -17,45 +17,94 @@ function change_furniture_tracker()
 function get_next_furnitures()
 {
 //gb_max_furniture_slider
-var furniture_1 = "<div class='large-3 medium-3 "
-var furniture_2 = "columns' style='margin-left:20px;' onclick=draw_scene(";
-var furniture_25 =  ",'furniture') ><img class='thumbnail' src=";
-var furniture_3 = " alt='";
-var furniture_4 = "'></div>";
 var furniture_elt;
 var sliced_furniture = furnitures.slice(gb_max_furniture_slider+1,gb_max_furniture_slider+4);
+var img_elt;
 for (furniture_elt in sliced_furniture)
 {
     console.log(furniture_elt);
     console.log(sliced_furniture[furniture_elt].path);
-    if (furniture_elt == gb_min_furniture_slider)
-    {
-    console.log('last one here');
-    $(".last-slider-"+gb_min_furniture_slider).after(furniture_1.concat(" last-slider-",String(gb_min_furniture_slider+3)," ",furniture_2,sliced_furniture[furniture_elt].name,"'",furniture_25,sliced_furniture[furniture_elt].path,furniture_3+sliced_furniture[furniture_elt].name,furniture_4));
-    }
-    else{
-    $(".last-slider-"+gb_min_furniture_slider).after(furniture_1.concat(furniture_2,"'",sliced_furniture[furniture_elt].name,"'",furniture_25,sliced_furniture[furniture_elt].path,furniture_3,sliced_furniture[furniture_elt].name,furniture_4));
-    }
-
-
+    img_elt = $("#furniture_slider").children()[parseInt(furniture_elt)+1].children[0];
+    img_elt.src = furnitures[gb_max_furniture_slider+parseInt(furniture_elt)+1].path;
+    img_elt.alt = furnitures[gb_max_furniture_slider+parseInt(furniture_elt)+1].name;
+    //$("#furniture_slider").children()[parseInt(furniture_elt)+1].onclick = null;
+    $("#furniture_slider").children()[parseInt(furniture_elt)+1].setAttribute('onclick', "draw_scene('".concat(furnitures[gb_max_furniture_slider+parseInt(furniture_elt)+1].name,"','furniture');"));
+    //function() {draw_scene(furnitures[gb_max_furniture_slider+parseInt(furniture_elt)+1].name,'furniture')};
 
 }
 
-for (furniture_elt in furnitures.slice(gb_min_furniture_slider,gb_max_furniture_slider+1))
-{
-$("#furniture_slider").children()[parseInt(furniture_elt)+gb_min_furniture_slider+1].style.display='none';
-}
 //update min and max
 gb_max_furniture_slider+=3;
 gb_min_furniture_slider+=3;
 
-//remove the class last slider
-//$(".last-slider")[0].className=$(".last-slider")[0].className.replace('last-slider','');
 }
+
+
+function get_next_pegs()
+{
+//gb_max_peg_slider
+var peg_elt;
+var sliced_peg = pegs.slice(gb_max_peg_slider+1,gb_max_peg_slider+9);
+var img_elt;
+for (peg_elt in sliced_peg)
+{
+    console.log(peg_elt);
+    console.log(sliced_peg[peg_elt].path);
+    img_elt = $("#peg_slider").children()[parseInt(peg_elt)+1].children[0];
+    img_elt.src = pegs[gb_max_peg_slider+parseInt(peg_elt)+1].path;
+    img_elt.alt = pegs[gb_max_peg_slider+parseInt(peg_elt)+1].name;
+    $("#peg_slider").children()[parseInt(peg_elt)+1].addEventListener('click', function(){ draw_scene(pegs[gb_min_furniture_slider+parseInt(peg_elt)+1].name,'peg');}, false);
+    //function() {draw_scene(furnitures[gb_max_furniture_slider+parseInt(furniture_elt)+1].name,'furniture')};
+
+}
+
+//update min and max
+gb_max_peg_slider+=8;
+gb_min_peg_slider+=8;
+
+}
+
+function get_previous_pegs()
+{
+
+//gb_max_furniture_slider
+var peg_elt;
+var sliced_peg = pegs.slice(gb_min_peg_slider-8,gb_min_peg_slider);
+for (peg_elt in sliced_peg)
+{
+    console.log(peg_elt);
+    console.log(sliced_peg[peg_elt].path);
+    $("#peg_slider").children()[parseInt(peg_elt)+1].children[0].src=pegs[gb_min_peg_slider-(8-parseInt(peg_elt))].path
+
+}
+
+//update min and max
+gb_max_peg_slider-=8;
+gb_min_peg_slider-=8;
+
+}
+
+
 
 function get_previous_furnitures()
 {
-gb_max_furniture_slider
+
+//gb_max_furniture_slider
+var furniture_elt;
+var sliced_furniture = furnitures.slice(gb_min_furniture_slider-3,gb_min_furniture_slider);
+for (furniture_elt in sliced_furniture)
+{
+    console.log(furniture_elt);
+    console.log(sliced_furniture[furniture_elt].path);
+    $("#furniture_slider").children()[parseInt(furniture_elt)+1].setAttribute('onclick', "draw_scene('".concat(furnitures[gb_min_furniture_slider-(3-parseInt(furniture_elt))].name,"','furniture');"));
+    $("#furniture_slider").children()[parseInt(furniture_elt)+1].children[0].src=furnitures[gb_min_furniture_slider-(3-parseInt(furniture_elt))].path
+
+}
+
+//update min and max
+gb_max_furniture_slider-=3;
+gb_min_furniture_slider-=3;
+
 }
 
 
